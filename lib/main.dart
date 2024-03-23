656b32ccecb5ef5434fa603b0263d6f9af9dae4d
Edil-Ayehu <125575950+Edil-Ayehu@users.noreply.github.com>
2024-03-23 18:49:42 +0300

    progress indicator & modal bottom sheet added!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import 'firebase_options.dart';
 
@@ -55,30 +56,38 @@ class _FirestoreInsertDemoState extends State<FirestoreInsertDemo> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final Product product = Product();
 
-  final TextEditingController _categoryIdController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  final TextEditingController _productTypeController = TextEditingController();
-  final TextEditingController _skuController = TextEditingController();
-  final TextEditingController _thumbnailController = TextEditingController();
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _priceController = TextEditingController();
-  final TextEditingController _salePriceController = TextEditingController();
-  final TextEditingController _stockController = TextEditingController();
-  final TextEditingController _isFeaturedController = TextEditingController();
-  final TextEditingController _imagesController = TextEditingController();
-  final TextEditingController _brandIdController = TextEditingController();
-  final TextEditingController _brandImageController = TextEditingController();
-  final TextEditingController _brandNameController = TextEditingController();
-  final TextEditingController _brandIsFeaturedController = TextEditingController();
-  final TextEditingController _productsCountController = TextEditingController();
+  final _categoryIdController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _productTypeController = TextEditingController();
+  final _skuController = TextEditingController();
+  final _thumbnailController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _priceController = TextEditingController();
+  final _salePriceController = TextEditingController();
+  final _stockController = TextEditingController();
+  final _isFeaturedController = TextEditingController();
+  final _imagesController = TextEditingController();
+
+  final _brandIdController = TextEditingController();
+  final _brandImageController = TextEditingController();
+  final _brandNameController = TextEditingController();
+  final _brandIsFeaturedController = TextEditingController();
+  final _productsCountController = TextEditingController();
 
   List<Map<String, dynamic>> _attributes = [];
   List<Map<String, dynamic>> _variations = [];
 
-  void _submitForm() {
+   bool _showSpinner = false; // Variable to control progress indicator
+
+  void _submitForm() async {
     final form = _formKey.currentState;
     if (form != null && form.validate()) {
       form.save();
+
+      setState(() {
+        _showSpinner = true; // Show progress indicator
+      });
+
       product.categoryId = _categoryIdController.text;
       product.description = _descriptionController.text;
       product.productType = _productTypeController.text;
@@ -100,11 +109,21 @@ class _FirestoreInsertDemoState extends State<FirestoreInsertDemo> {
       product.productAttributes = _attributes;
       product.productVariations = _variations;
 
-      _insertProduct(product);
+      bool success = await _insertProduct(product);
+
+      // Show modal bottom sheet indicating success or failure
+      _showResultModal(success);
+
+       setState(() {
+        _showSpinner = false; // Hide progress indicator
+      });
+
+      // Clear input fields
+      _clearInputFields();
     }
   }
 
-  Future<void> _insertProduct(Product product) async {
+  Future<bool> _insertProduct(Product product) async {
     try {
       // Get Firestore instance
       FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -113,9 +132,74 @@ class _FirestoreInsertDemoState extends State<FirestoreInsertDemo> {
       await firestore.collection('Products').add(product.toMap());
 
       debugPrint('Product inserted successfully!');
+      return true; // Return true for success
     } catch (error) {
       debugPrint('Error inserting product: $error');
+      return false; // Return false for failure
     }
+  }
+
+  void _showResultModal(bool success) {
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      showDragHandle: true,
+      context: context,
+      builder: (context) {
+        return Center(
+          child: Container(
+            padding: const EdgeInsets.all(36.0),
+            margin: const EdgeInsets.symmetric(horizontal: 16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  success ? Icons.check_circle : Icons.error,
+                  color: success ? Colors.green : Colors.red,
+                  size: 36.0,
+                ),
+                const SizedBox(height: 12.0),
+                Text(
+                  success
+                      ? 'Product inserted successfully!'
+                      : 'Failed to insert product!',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: success ? Colors.green : Colors.red,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _clearInputFields() {
+    _categoryIdController.clear();
+    _descriptionController.clear();
+    _productTypeController.clear();
+    _skuController.clear();
+    _thumbnailController.clear();
+    _titleController.clear();
+    _priceController.clear();
+    _salePriceController.clear();
+    _stockController.clear();
+    _isFeaturedController.clear();
+    _imagesController.clear();
+    _brandIdController.clear();
+    _brandImageController.clear();
+    _brandNameController.clear();
+    _brandIsFeaturedController.clear();
+    _productsCountController.clear();
+    _attributes = [];
+    _variations.clear();
   }
 
   @override
@@ -124,99 +208,104 @@ class _FirestoreInsertDemoState extends State<FirestoreInsertDemo> {
       appBar: AppBar(
         title: const Text('Firestore Insert Demo'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                controller: _categoryIdController,
-                decoration: const InputDecoration(labelText: 'Category ID'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter category ID';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(labelText: 'Description'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter description';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _productTypeController,
-                decoration: const InputDecoration(labelText: 'Product Type'),
-              ),
-              TextFormField(
-                controller: _skuController,
-                decoration: const InputDecoration(labelText: 'SKU'),
-              ),
-              TextFormField(
-                controller: _thumbnailController,
-                decoration: const InputDecoration(labelText: 'Thumbnail'),
-              ),
-              TextFormField(
-                controller: _titleController,
-                decoration: const InputDecoration(labelText: 'Title'),
-              ),
-              TextFormField(
-                controller: _priceController,
-                decoration: const InputDecoration(labelText: 'Price'),
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                controller: _salePriceController,
-                decoration: const InputDecoration(labelText: 'Sale Price'),
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                controller: _stockController,
-                decoration: const InputDecoration(labelText: 'Stock'),
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                controller: _isFeaturedController,
-                decoration: const InputDecoration(labelText: 'Is Featured'),
-              ),
-              TextFormField(
-                controller: _imagesController,
-                decoration: const InputDecoration(labelText: 'Images (comma-separated)'),
-              ),
-              TextFormField(
-                controller: _brandIdController,
-                decoration: const InputDecoration(labelText: 'Brand ID'),
-              ),
-              TextFormField(
-                controller: _brandImageController,
-                decoration: const InputDecoration(labelText: 'Brand Image'),
-              ),
-              TextFormField(
-                controller: _brandNameController,
-                decoration: const InputDecoration(labelText: 'Brand Name'),
-              ),
-              TextFormField(
-                controller: _brandIsFeaturedController,
-                decoration: const InputDecoration(labelText: 'Brand IsFeatured'),
-              ),
-              TextFormField(
-                controller: _productsCountController,
-                decoration: const InputDecoration(labelText: 'Products Count'),
-                keyboardType: TextInputType.number,
-              ),
-              _buildAttributesInput(),
-              _buildVariationsInput(),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: const Text('Insert Product'),
-              ),
-            ],
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  controller: _categoryIdController,
+                  decoration: const InputDecoration(labelText: 'Category ID'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter category ID';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(labelText: 'Description'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter description';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _productTypeController,
+                  decoration: const InputDecoration(labelText: 'Product Type'),
+                ),
+                TextFormField(
+                  controller: _skuController,
+                  decoration: const InputDecoration(labelText: 'SKU'),
+                ),
+                TextFormField(
+                  controller: _thumbnailController,
+                  decoration: const InputDecoration(labelText: 'Thumbnail'),
+                ),
+                TextFormField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                TextFormField(
+                  controller: _priceController,
+                  decoration: const InputDecoration(labelText: 'Price'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextFormField(
+                  controller: _salePriceController,
+                  decoration: const InputDecoration(labelText: 'Sale Price'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextFormField(
+                  controller: _stockController,
+                  decoration: const InputDecoration(labelText: 'Stock'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextFormField(
+                  controller: _isFeaturedController,
+                  decoration: const InputDecoration(labelText: 'Is Featured'),
+                ),
+                TextFormField(
+                  controller: _imagesController,
+                  decoration: const InputDecoration(
+                      labelText: 'Images (comma-separated)'),
+                ),
+                TextFormField(
+                  controller: _brandIdController,
+                  decoration: const InputDecoration(labelText: 'Brand ID'),
+                ),
+                TextFormField(
+                  controller: _brandImageController,
+                  decoration: const InputDecoration(labelText: 'Brand Image'),
+                ),
+                TextFormField(
+                  controller: _brandNameController,
+                  decoration: const InputDecoration(labelText: 'Brand Name'),
+                ),
+                TextFormField(
+                  controller: _brandIsFeaturedController,
+                  decoration:
+                      const InputDecoration(labelText: 'Brand IsFeatured'),
+                ),
+                TextFormField(
+                  controller: _productsCountController,
+                  decoration: const InputDecoration(labelText: 'Products Count'),
+                  keyboardType: TextInputType.number,
+                ),
+                _buildAttributesInput(),
+                _buildVariationsInput(),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  child: const Text('Insert Product'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
